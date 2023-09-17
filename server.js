0afc04887b5199e359eb631be4907f61dbe6406c
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-09-17 15:09:25 -0500

    removed a line

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require('nodemailer');
-const PORT = process.env.PORT|| 5000;
 
 // these here are the servers used to send emails to well.. ME
 const app = express();
