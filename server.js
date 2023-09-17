35c43c72e75054e0c0ef6c70b50b36e9891a4ce7
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-09-17 02:15:26 -0500

    i think mail should work this time

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3001, () => console.log("Server running is good"));
+app.listen(5000, () => console.log("Server running is good"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: "********@gmail.com",
-        pass: ""
+        user: "lucaswyski01@gmail.com",
+        pass: "qmnsusyhcbwhprno"
     },
 });
 
